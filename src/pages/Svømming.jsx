99957d8c5f0267d6700e17807935896925aee1bd
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:07:21 +0100

    Create Svømming.jsx

---
+++ src/pages/Svømming.jsx
@@ -0,0 +1,8 @@
+export default function Svømming() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Svømming</h2>
+      <p>Her kommer informasjon om svømmekonkurransen.</p>
+    </div>
+  );
+}
