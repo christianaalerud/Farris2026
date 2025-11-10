57c42e0de5ea212261ca9e1e9b464c3d44108382
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:10:12 +0100

    Create FAQ.jsx

---
+++ src/pages/FAQ.jsx
@@ -0,0 +1,8 @@
+export default function FAQ() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>FAQ</h2>
+      <p>Ofte stilte spørsmål</p>
+    </div>
+  );
+}
