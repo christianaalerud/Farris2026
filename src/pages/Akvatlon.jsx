b10c1d9c09ddc893a420b501cb354a7d8e94a108
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:05:41 +0100

    Create Akvatlon.jsx

---
+++ src/pages/Akvatlon.jsx
@@ -0,0 +1,8 @@
+export default function Akvatlon() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Akvatlon</h2>
+      <p>Her kommer informasjon om Akvatlon-konkurransen.</p>
+    </div>
+  );
+}
