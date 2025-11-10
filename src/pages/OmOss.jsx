ab150fa43f6ee7668cf74b8c1bc76e4f12d267ba
christianaalerud <christianaalerud@gmail.com>
2025-11-10 19:20:31 +0100

    Update OmOss.jsx

--- src/pages/OmOss.jsx
+++ src/pages/OmOss.jsx
@@ -2,7 +2,19 @@ export default function OmOss() {
   return (
     <div style={{ padding: "60px 20px", textAlign: "center" }}>
       <h2>Om Oss</h2>
-      <p>Vi er to drømmere som ønsker å skape et event ingen av oss kommer til å glemme!</p>
+      <p>Hei! Vi heter Christian og Iselin. Vi er to drømmere med en forkjærlighet for å få oppheng i nye prosjekter. De som kjenner oss godt vet at vi tidligere har vært innom mange rare oppheng og idéer, og nå har vi fått det for noe litt større enn vanlig, nemlig å arrangere et triatlon.
+
+Har vi noen gang deltatt i et selv? Nei.
+
+Stopper det oss fra å arrangere et? Absolutt ikke.
+
+Vi tenker som Obama: Yes we can.
+
+Vi vil samle kjente og kjære, og ukjente og nysgjerrige, til en helg fylt med mestring, moro og fellesskap. Dette er et lavterskel-triatlon hvor både spreke og uspreke er hjertelig velkomne. Her handler det ikke om å vinne, men om å være med, utfordre seg selv og ha det gøy sammen.
+
+Det skal bades, sykles og løpes, og det blir mulighet for overnatting for dem som ikke vil stresse hjem, men bli med videre på festen.
+
+Vi gleder oss til å utspille våre rare idéer med en god gjeng. Bli med, det blir gøy! </p>
     </div>
   );
 }
