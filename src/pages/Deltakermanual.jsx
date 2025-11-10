f72cfd650e6b2e5b3fd69dbbcedb1d6e053888ac
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:08:41 +0100

    Create Deltakermanual.jsx

---
+++ src/pages/Deltakermanual.jsx
@@ -0,0 +1,8 @@
+export default function Deltakermanual() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Deltakermanual</h2>
+      <p>Viktig info om regler og annet nyttig vil komme her.</p>
+    </div>
+  );
+}
