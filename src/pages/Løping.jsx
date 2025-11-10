bcc4d68e5f78979ac043b94d8df35383263e32e5
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:07:53 +0100

    Create Løping.jsx

---
+++ src/pages/Løping.jsx
@@ -0,0 +1,8 @@
+export default function Løping() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Løping</h2>
+      <p>Her kommer informasjon om løpekonkurransen.</p>
+    </div>
+  );
+}
