12cdd95fb368786ff3ef318c29dda13a7a1b8061
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:11:10 +0100

    Create OmOss.jsx

---
+++ src/pages/OmOss.jsx
@@ -0,0 +1,8 @@
+export default function Om Oss() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Om Oss</h2>
+      <p>Vi er to drømmere som ønsker å skape et event ingen av oss kommer til å glemme!</p>
+    </div>
+  );
+}
