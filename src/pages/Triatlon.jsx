d9e86fac6fb43da1ccd1adcbaf8d956df8d43258
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:04:39 +0100

    Create Triatlon.jsx

---
+++ src/pages/Triatlon.jsx
@@ -0,0 +1,8 @@
+export default function Triatlon() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Triatlon</h2>
+      <p>Her kommer informasjon om triatlon-konkurransen.</p>
+    </div>
+  );
+}
