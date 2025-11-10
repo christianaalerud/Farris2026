a3fbb5920d863ceca0a538e4ba972790c8063b1f
christianaalerud <christianaalerud@gmail.com>
2025-11-10 12:09:30 +0100

    Create Løypeprofiler.jsx

---
+++ src/pages/Løypeprofiler.jsx
@@ -0,0 +1,8 @@
+export default function Løypeprofiler() {
+  return (
+    <div style={{ padding: "60px 20px", textAlign: "center" }}>
+      <h2>Løypeprofiler</h2>
+      <p>Her kommer informasjon om de ulike løypeprofilene for både svømming, sykling og løping.</p>
+    </div>
+  );
+}
